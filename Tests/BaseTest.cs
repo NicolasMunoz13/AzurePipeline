eb8dd03761617c317acc77d47427b39bd0ed1493
NicolasMunoz13 <juanicolasmuozcampos@gmail.com>
2023-03-07 13:23:18 -0500

    First commit

---
+++ Tests/BaseTest.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1.Tests
+{
+    [TestClass]
+    public class BaseTest
+    {
+        private IWebDriver driver;
+
+        //[TestInitialize]
+        public void SetUp(String browser)
+        {
+
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
